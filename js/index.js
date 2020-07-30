aed66a5528375b1cf669c7455d43e9ad9b466d35
Alex Oliveira <alex_oliveira08@yahoo.com.br>
2020-07-30 18:00:31 -0300

    Iframe test youtube

---
+++ js/index.js
@@ -0,0 +1,39 @@
+// Videos
+let magic = 'https://www.youtube.com/embed/Qtb11P1FWnc'
+let paradise = 'https://www.youtube.com/embed/1G4isv_Fylg'
+let sky = 'https://www.youtube.com/embed/uREb6T4Tsgk'
+let adventure = 'https://www.youtube.com/embed/vzHwYQJ9cCA'
+let vivaLaVida = 'https://www.youtube.com/embed/vUnWFsc5WBU'
+
+// Definindo objeto iframe
+let iframe = document.querySelector("#destaque iframe")
+
+
+// function para exibir video
+function exibeVideo(numero) {
+
+    switch (numero) {
+        case 1:
+            iframe.setAttribute("src", magic)
+            break;
+        case 2:
+            iframe.setAttribute("src", paradise)
+            break;
+        case 3:
+            iframe.setAttribute("src", sky)
+            break;
+        case 4:
+            iframe.setAttribute("src", adventure)
+            break;
+        case 5:
+            iframe.setAttribute("src", vivaLaVida)
+            break;
+
+    }
+
+    // definindo atributo
+
+
+
+
+}
